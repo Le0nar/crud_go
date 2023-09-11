9b5c1d212757bd69625eef17c175f322d00e6492
Le0nar <yaroslavtrefilov5@gmail.com>
2023-09-11 23:33:44 +0400

    feat: initial repository, service and handler in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	news "github.com/Le0nar/crud_go"
 	"github.com/Le0nar/crud_go/pkg/handler"
+	"github.com/Le0nar/crud_go/pkg/repository"
+	"github.com/Le0nar/crud_go/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handler := handler.NewHandler(service)
 
 	srv := new(news.Server)
 	if err := srv.Run("8000", handler.InitRoutes()); err != nil {

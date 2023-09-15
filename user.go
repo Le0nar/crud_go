4386ae7cf39ab7289831fbd8380cc15c15913575
Le0nar <yaroslavtrefilov5@gmail.com>
2023-09-15 10:54:24 +0400

    feat: add authentication for registered user

--- user.go
+++ user.go
@@ -1,7 +1,7 @@
 package news
 
 type User struct {
-	Id       int    `json:"-"`
+	Id       int    `json:"-" db:"id"`
 	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
